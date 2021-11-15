e327d5f86a8a233ea2f489f0a3e61e33d81f5a53
Ícaro Almeida <progalmeidaregis@outlook.com>
2021-11-14 22:11:33 -0300

    task01

---
+++ tasks/01Task.js
@@ -0,0 +1,24 @@
+// Extraia partes do comando como o nome da tabela e as colunas, armazenando-as em variáveis.
+
+// # Instruções
+
+// Dado o comando (uma *string*):
+
+// create table author (id number, name string, age number, city string, state string, country string)
+
+// ```
+// create table author (id number, name string, age number, city string, state string, country string)
+// ```
+
+// 1. Extraia o nome da tabela e armazene em uma variável chamada **tableName.**
+// 2. Extraia as colunas da tabela e armazene em uma variável chamada **columns.**
+// 3. Manipule a variável **columns**, separando cada coluna com seu respectivo tipo, em uma string separada.
+
+const dataTable =
+  'create table author (id number, name string, age number, city string, state string, country string)';
+
+//1 questão
+const tableName = dataTable.substring(13, 19);
+console.log(tableName);
+
+//2 questão
