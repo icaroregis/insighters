cd759ca9c6934854cdc3c9e95cbf293d4b331f5c
Ícaro Almeida <progalmeidaregis@outlook.com>
2021-11-24 21:41:56 -0300

    ...

--- tasks/03Task.js
+++ tasks/03Task.js
@@ -6,47 +6,52 @@
 // 2. Mova o código responsável por criar a tabela para dentro do método **createTable**.
 // 3. Crie uma função chamada **execute**, invocando dinamicante a função **createTable**.
 
-//task01
-const dataTable =
-  'create table author (id number, name string, age number, city string, state string, country string)';
-
-const regex = /([a-z]+) \((.+)\)/;
-const result = regex.exec(dataTable);
-const tableName = result[1];
-console.log(tableName);
-
-const columns = result[2];
-const resultColums = columns.split(', ');
-console.log(resultColums);
-
-//task02
-function creatorTable() {
-  let transformarEmObject = Object.assign(
-    ...resultColums.map((column) => {
-      const object = column.split(' ');
-      let propriedade = object[0];
-      let valor = object[1];
-      return { [propriedade]: valor };
-    }),
-  );
-
-  let database = {
-    tables: {
-      [tableName]: {
-        columns: transformarEmObject,
-        data: [],
-      },
-    },
-  };
-}
+//   let database = {
+//     tables: {
+//       [tableName]: {
+//         columns: transformarEmObject,
+//         data: [],
+//       },
+//     },
+//   };
+// }
 
 //task 03
 let database = {
-  createTable(creatorTable) {
-    return tables;
+  tables: {
+    [tableName]: {
+      columns: transformarEmObject,
+      data: [],
+    },
   },
-  execute(createTable) {
-    return createTable;
+  creatorTable(statement) {
+    //task01
+    const dataTable =
+      'create table author (id number, name string, age number, city string, state string, country string)';
+
+    const regex = /([a-z]+) \((.+)\)/;
+    const result = regex.exec(dataTable);
+    const tableName = result[1];
+    console.log(tableName);
+
+    const columns = result[2];
+    const resultColums = columns.split(', ');
+    console.log(resultColums);
+
+    //task02
+    function creatorTable() {
+      let transformarEmObject = Object.assign(
+        ...resultColums.map((column) => {
+          const object = column.split(' ');
+          let propriedade = object[0];
+          let valor = object[1];
+          return { [propriedade]: valor };
+        }),
+      );
+    }
+  },
+  execute(statement) {
+    this.creatorTable(statement);
   },
 };
 
